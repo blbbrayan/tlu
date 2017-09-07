2de8f71b13e2b0e1f8e12d16edcbed8313118637
Grandmaster Brayan <brayanbyrdsong@gmail.com>
2017-09-07 12:57:35 -0400

    no message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.database.subscribe('entries', data=>{
+      data = data || {};
       this.entries = ObjectUtil.toArray(data);
     });
   }
